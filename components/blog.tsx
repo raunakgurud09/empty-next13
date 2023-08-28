666a448b9f7f4b3b38de3a37c6efaa1fbf497b85
Raunak Gurud <raunakgurud2121@gmail.com>
2023-08-28 23:26:45 +0530

    feat: added blog and experience components

---
+++ components/blog.tsx
@@ -0,0 +1,42 @@
+import { Link } from "lucide-react"
+
+export default function Blog() {
+  return (
+    <section className="flex flex-col w-full mx-auto space-y-4 drop-shadow-2xl">
+      <h3 className="text-2xl px-4">Blog</h3>
+      <div className="flex flex-col space-y-2">
+        {
+          [...Array(3)].map((_, i) => (
+            <div className="px-4" key={i}>
+              <BlogCard />
+            </div>
+          ))
+        }
+      </div>
+
+
+    </section>
+  )
+}
+
+function BlogCard() {
+  return (
+    <div className="py-4  px-0  flex flex-col md:flex-row hover:scale-[1.02] transition-all rounded-md">
+
+      <div className="w-full md:w-2/12 h-56 md:h-40 px-0 md:px-1">
+        {/* image */}
+        <div className="bg-white/10 w-full h-full rounded-md"></div>
+      </div>
+
+      <div className="w-full md:w-10/12 flex flex-col items-start justify-center  px-0 py-2 md:px-2 md:py-0">
+
+        <h4 className="text-lg font-bold">Title of the blog</h4>
+        <div className="flex flex-row space-x-4">
+          <p className="text-sm font-thin">23/08/2023</p>
+          <p className="text-sm font-thin">6 min read</p>
+        </div>
+        <p className="text-sm font-extralight mt-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita, sed officia iste aliquid architecto harum minus accusantium rem dicta voluptates?</p>
+      </div>
+    </div>
+  )
+}
---
+++ components/experience.tsx
@@ -0,0 +1,52 @@
+import { Link } from "lucide-react"
+
+const tools = [
+  "Next.js",
+  "Redux",
+  "Node"
+]
+
+
+export default function Experience() {
+  return (
+    <section className="flex flex-col w-full mx-auto space-y-4  drop-shadow-2xl">
+      <h3 className="text-2xl px-4">Experience</h3>
+      <div className="flex flex-col space-y-2">
+        {
+          [...Array(3)].map((_, i) => (
+            <div className="px-4" key={i}>
+              <ExperienceCard />
+            </div>
+          ))
+        }
+      </div>
+
+
+    </section>
+  )
+}
+
+function ExperienceCard() {
+  return (
+    <div className="flex flex-col md:flex-row mx-auto py-4 ease-in transition-all rounded-md">
+      <div className="w-full md:w-2/12 p-1 py-2 text-xs">Jun 2022 - ongoing</div>
+      <div className="w-full md:w-9/12 flex flex-col p-1 ">
+        <h4 className="text-lg font-bold">Title of position</h4>
+        <div className="flex items-center justify-start">
+          <Link size={12}/>
+          <p className="text-white/50 ml-2"> link to work</p>
+        </div>
+        <p className="text-sm font-extralight">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, fuga est, nam alias dolore sunt quo sit facere esse cumque debitis animi explicabo eum, eius quidem commodi quis repellendus excepturi necessitatibus sapiente deleniti ut ipsum sequi. Amet quod dolorem deserunt placeat, debitis corrupti delectus fugiat mollitia, laboriosam facilis officiis deleniti</p>
+        <div className="flex flex-row">
+          {
+            tools.map((t, i) =>
+              <p key={i} className="text-sm">
+                {i != 0 && <span className="mx-1">•</span>}
+                {t} </p>
+            )
+          }
+        </div>
+      </div>
+    </div>
+  )
+}
