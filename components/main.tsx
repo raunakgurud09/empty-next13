7cd497b675fd0882c0d764a097ebee6666421626
Raunak Gurud <raunakgurud2121@gmail.com>
2023-08-29 21:30:09 +0530

    fix: making main responsive

--- components/main.tsx
+++ components/main.tsx
@@ -7,7 +7,7 @@ import Socials from './socials'
 
 export default function Main() {
   return (
-    <section className='flex flex-row w-full mx-auto space-y-4 px-4 mt-32 mb-20 '>
+    <section className='flex flex-col-reverse md:flex-row w-full mx-auto space-y-4 px-4 mt-32 mb-20 '>
       <div className="absolute top-0 left-0 right-0">
         <div className="absolute left-10 md:left-32 top-1 -z-10 overflow-visible opacity-20">
           <div className="circle-1 -z-10 h-[400px] w-[200px] rounded-full bg-cyan-400 mix-blend-multiply blur-[128px]" />
@@ -17,7 +17,7 @@ export default function Main() {
         </div>
       </div>
 
-      <div className='w-3/4'>
+      <div className='w-full md:w-3/4'>
         <h1 className=' text-5xl '>Raunak Gurud</h1>
         <p className='flex'>
           <span className='p-1'>
@@ -30,7 +30,7 @@ export default function Main() {
           </div>
         </p>
         <br />
-        <p>
+        <p className='font-light'>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem laudantium molestiae enim, autem, placeat, dolor mollitia commodi tempore numquam laboriosam recusandae! Eligendi commodi aperiam corporis iste esse ratione illo ad.
         </p>
         <div className='mt-10'>
@@ -38,13 +38,13 @@ export default function Main() {
         </div>
 
       </div>
-      <div className=''>
+      <div className='w-full md:w-fit flex justify-start items-start'>
         <NextImage
           src={profile_image}
           alt='profile'
           width={224}
           height={224}
-          className='h-56 w-56 rounded-full backdrop-blur-sm shadow-cyan-400 shadow-lg bg-black'
+          className='h-56 w-56 rounded-full backdrop-blur-sm shadow-orange-400 shadow-lg bg-black'
         />
       </div>
     </section >
