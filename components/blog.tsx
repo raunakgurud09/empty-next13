8520cc7f1ab2fbbeebb16ce0af74ad4610ce323d
Raunak Gurud <raunakgurud2121@gmail.com>
2023-08-31 15:14:06 +0530

    feat: dynamic data for blogs added

--- components/blog.tsx
+++ components/blog.tsx
@@ -1,4 +1,6 @@
-import { Link } from "lucide-react"
+import { posts } from "@/data/hashnode.json"
+import NextImage from "next/image"
+import { format, parseISO } from "date-fns"
 
 export default function Blog() {
   return (
@@ -6,9 +8,17 @@ export default function Blog() {
       <h3 className="text-2xl font-bold px-4">Blog</h3>
       <div className="flex flex-col space-y-2">
         {
-          [...Array(2)].map((_, i) => (
+          posts.map((post, i) => (
             <div className="px-4" key={i}>
-              <BlogCard />
+              <BlogCard
+                _id={post._id}
+                slug={post.slug}
+                title={post.title}
+                brief={post.brief}
+                coverImage={post.coverImage}
+                readingTime={post.readingTime.minutes}
+                dateAdded={post.dateAdded}
+              />
             </div>
           ))
         }
@@ -17,22 +27,45 @@ export default function Blog() {
   )
 }
 
-function BlogCard() {
+type Props = {
+  _id: string;
+  slug: string;
+  title: string;
+  brief: string;
+  coverImage: string;
+  dateAdded: string;
+  readingTime: number
+}
+
+function BlogCard({
+  _id,
+  slug,
+  title,
+  brief,
+  coverImage,
+  dateAdded,
+  readingTime,
+}: Props) {
   return (
     <div className="py-4  px-0  flex flex-col md:flex-row hover:scale-[1.02] transition-all rounded-md">
       <div className="w-full md:w-2/12 h-56 md:h-28 px-0 md:pr-1">
         {/* image */}
-        <div className="bg-white/10 w-full h-full rounded-md"></div>
+        <NextImage
+          src={coverImage}
+          alt={slug}
+          width={144}
+          height={256}
+          className="bg-white/10 w-full h-full rounded-md object-cover" />
       </div>
 
       <div className="w-full md:w-10/12 flex flex-col items-start justify-center  px-0 py-2 md:px-2 md:py-0">
 
-        <h4 className="text-lg font-semibold">Title of the blog</h4>
+        <h4 className="text-lg font-semibold">{title}</h4>
         <div className="flex flex-row space-x-4">
-          <p className="text-sm font-light">23/08/2023</p>
-          <p className="text-sm font-light">6 min read</p>
+          <p className="text-sm font-light">{format(parseISO(dateAdded), "PPP")}</p>
+          <p className="text-sm font-light">{Math.floor(readingTime)} min read</p>
         </div>
-        <p className="text-sm font-light mt-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita, sed officia iste aliquid architecto harum minus accusantium rem dicta voluptates?</p>
+        <p className="text-sm font-light mt-2 ">{brief}</p>
       </div>
     </div>
   )
