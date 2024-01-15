745a73f8b73f4ab1750ae6162609b5b2a277fc52
Raunak Gurud <raunakgurud2121@gmail.com>
2024-01-15 21:11:14 +0530

    refactor: default theme to dark

--- components/provider.js
+++ components/provider.js
@@ -3,5 +3,5 @@
 import { ThemeProvider } from "next-themes";
 
 export default function Provider({ children }) {
-  return <ThemeProvider defaultTheme="light">{children}</ThemeProvider>;
+  return <ThemeProvider defaultTheme="dark">{children}</ThemeProvider>;
 }
